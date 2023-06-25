7291201e06b4c642cefd0eb7009410a452cceb36
Zedd <zeddnyx08@gmail.com>
2023-06-26 06:16:06 +0700

    add favicon, etc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,29 @@
-import './styles/globals.css'
-import { Inter } from 'next/font/google'
+import { Metadata } from "next";
+import "./styles/globals.css";
+import { Inter } from "next/font/google";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: 'Blue Studio',
-  description: 'Unleashing the magic of illustration and animation',
-}
+export const metadata: Metadata = {
+  title: "Blue Studio",
+  description: "Unleashing the magic of illustration and animation",
+  icons: {
+    icon: "../public/favicon-16x16.png"
+  }
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="../public/favicon-16x16.png"/>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
